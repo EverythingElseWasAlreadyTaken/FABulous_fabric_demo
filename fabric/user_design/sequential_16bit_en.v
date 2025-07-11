f685dd8799b915f4c92160a38171d4e8890173cc
Jonas K. <j0nask@proton.me>
2025-07-11 14:36:42 +0200

    Add generated fabric and cad integration files.
    Run FABulous fabric generation and add fabric files.
    Add nextpnr model and bitStreamSpec.

--- fabric/user_design/sequential_16bit_en.v
+++ fabric/user_design/sequential_16bit_en.v
@@ -1,7 +1,7 @@
 module sequential_16bit_en(input wire clk, input wire [15:0] io_in, output wire [15:0] io_out, io_oeb);
     wire rst = io_in[0];
     wire en = io_in[1];
-    reg [13:0] ctr;
+    reg [25:0] ctr;
 
     always @(posedge clk)
         if (en)
@@ -12,6 +12,6 @@ module sequential_16bit_en(input wire clk, input wire [15:0] io_in, output wire 
         else
             ctr <= ctr;
 
-    assign io_out = {2'b0, ctr}; // pass thru reset for debugging
+    assign io_out = ctr[25:10]; // pass thru reset for debugging
     assign io_oeb = 16'b1;
 endmodule
