0761e6f0e3f610487027a695db4ca4562172932a
Jonas K. <j0nask@proton.me>
2025-08-14 18:30:27 +0200

    Fix user design for simulation
    Signed-off-by: Jonas K. <j0nask@proton.me>

--- fabric/user_design/sequential_16bit_en.v
+++ fabric/user_design/sequential_16bit_en.v
@@ -12,6 +12,7 @@ module sequential_16bit_en(input wire clk, input wire [15:0] io_in, output wire 
         else
             ctr <= ctr;
 
-    assign io_out = ctr[25:10]; // pass thru reset for debugging
+    // assign io_out = ctr[25:10]; // only for FPGA emulation to get some blinky
+    assign io_out = ctr[15:0]; // only for simulation since we run it only 100 cylces
     assign io_oeb = 16'b1;
 endmodule
